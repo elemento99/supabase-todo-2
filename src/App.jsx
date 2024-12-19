c04485e6b9dae31a4d8ad7ab386cea136e9979d3
hajime2 <joaquin.gonzalezparada@gmail.com>
2024-12-19 08:59:24 -0300

    hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
-import supabase from './supabase-client'
-import './App.css'
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import { TaskContextProvider } from './context/TaskContext'
-import Navbar from './components/Navbar'
-
-
-import Login from './components/login'
-
-import Home from './pages/Home'
-import NotFound from './pages/NotFound'
-
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { TaskContextProvider } from './context/TaskContext';
+import Navbar from './components/Navbar';
+import Login from './components/login';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
-    const navigate = useNavigate()
-
-
-
     return (
-        <div className="App">       
-         <TaskContextProvider>
-            <Navbar/>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </TaskContextProvider></div>
-
-    )
+        <div className="App">
+            <TaskContextProvider>
+                <Router>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Router>
+            </TaskContextProvider>
+        </div>
+    );
 }
 
-export default App
+export default App;
+
